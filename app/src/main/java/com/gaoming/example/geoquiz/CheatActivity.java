fb8407e35b4d64cae93a612761e7f93455b85e4d
101003111 <gaoming@huaqin.com>
2017-05-09 17:00:55 +0800

    添加版本兼容判断

--- app/src/main/java/com/gaoming/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/gaoming/example/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.gaoming.example.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +22,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private boolean mIsShowAnswer = false;
     private TextView mAnswerTextView;
+    private TextView mApiTextView;
     private Button mShowAnswer;
 
     public static boolean wasAnswerShown(Intent result){
@@ -30,12 +35,34 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
         mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mApiTextView = (TextView) findViewById(R.id.apiLevelTextView);
+        mApiTextView.append(" "+Build.VERSION.SDK_INT);
+//        mApiTextView.setText("API LEVEL: " + Build.VERSION.SDK_INT);
+
         mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mIsShowAnswer = true;
                 showAnswer();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, 0, radius);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.VISIBLE);
+                        }
+                    });
+                    anim.start();
+                }else{
+                    mShowAnswer.setVisibility(View.VISIBLE);
+                }
             }
         });
         if (savedInstanceState != null){
